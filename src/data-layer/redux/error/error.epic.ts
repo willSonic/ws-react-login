01d6e4dedfee060963d069fc991b5f5a9f755acd
Willie Streeter <willie.l.streeter@kp.org>
2017-11-03 15:58:21 -0700

    update

--- src/data-layer/redux/error/error.epic.ts
+++ src/data-layer/redux/error/error.epic.ts
@@ -1,55 +1,44 @@
 import { Observable } from 'rxjs/Observable';
+import { ActionsObservable } from 'redux-observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
 
 import { ErrorModel } from '../../../business-layer/models/error.model';
+import { browserHistory } from 'react-router';
 
 import * as errorActions from './error.actions';
-import * as profileActions from '../actions/profile.actions';
-import * as usersessionActions from '../actions/usersession.actions';
+import * as profileActions from '../profile/profile.actions';
+import * as userSessionActions from '../user-session/usersession.actions';
 
 
 
 export default class ErrorEpic {
 
 
- @Effect() catchAllRemoteError$ = this.actions$
-  .ofType(errorActions.ErrorTypes.REPORT_ERROR)
-  .map(action => action.payload)
-  .switchMap(payload => {
-    let obs;
+        static catchAllRemoteErros = ( action$: ActionsObservable<any>) =>
+                action$.ofType(errorActions.REPORT_ERROR)
+                    .switchMap(({ payload }) => {
+                        let obs;
+                        switch(payload.action_type) {
 
-    switch(payload.action_type) {
+                             case profileActions.CHECK_USER_PROFILE_NAME_FAILURE:
+                              if(this.router.url.indexOf('register')>0) {
+                                obs = Observable.of( new profileActions.CheckUserProfileNameFailure(<ErrorModel> payload));
+                              }else {
+                                obs = Observable.of(browserHistory.push('/error'));
+                              }
+                             break;
 
-     case profileActions.ProfileTypes.CHECK_USER_PROFILE_NAME_FAILURE:
-      if(this.router.url.indexOf('register')>0) {
-        obs = Observable.of( new profileActions.CheckUserProfileNameFailure(<ErrorModel> payload));
-      }else {
-        obs = Observable.of(this.router.navigateByUrl('/error'));
-      }
-     break;
+                             case userSessionActions.LOGIN_USER_FAILURE:
+                               obs = Observable.of(new userSessionActions.UserLoginFailure(<ErrorModel> payload));
+                             break;
 
-     case usersessionActions.UserSessionTypes.LOGIN_USER_FAILURE:
-       obs = Observable.of(new usersessionActions.UserLoginFailure(<ErrorModel> payload));
-     break;
-
-     default:{
-       obs = Observable.of(this.router.navigateByUrl('/error'));
-     }
-
-
-    }
-    return obs;
-  });
-
-
-
-    constructor(
-        private actions$: Actions,
-        private router: Router
-
-    ) {  }
-
+                             default:{
+                               obs = Observable.of(this.router.navigateByUrl('/error'));
+                             }
+                        }
+                        return obs;
+                   });
 
 }
